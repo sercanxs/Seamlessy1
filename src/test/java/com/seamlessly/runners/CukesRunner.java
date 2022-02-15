c93698a882b8abb78fb38c025553b5b73d1d73ce
Baldwin <sercanxssahin@hotmail.com>
2022-02-15 12:13:36 +0300

    pages1

--- src/test/java/com/seamlessly/runners/CukesRunner.java
+++ src/test/java/com/seamlessly/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/seamlessly/stepdefinitions",
         dryRun = false,
-        tags = "@190"
+        tags = "@smoke"
 )
 public class CukesRunner {
 }

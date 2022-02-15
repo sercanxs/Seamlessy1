935a86f10446a7dd2619f2b8d704f3f476427903
Baldwin <sercanxssahin@hotmail.com>
2022-02-15 09:36:19 +0300

    pages1

---
+++ src/test/java/com/seamlessly/pages/BasePage.java
@@ -0,0 +1,21 @@
+package com.seamlessly.pages;
+
+import com.seamlessly.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/seamlessly/stepdefinitions/Loginpage.java
+++ src/test/java/com/seamlessly/stepdefinitions/Loginpage.java
@@ -1,5 +1,42 @@
 package com.seamlessly.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Loginpage {
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters the {string} {string}")
+    public void the_user_enters_the(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Click the login button")
+    public void click_the_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify that user should able to lands on the dashboard page")
+    public void verify_that_user_should_able_to_lands_on_the_dashboard_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Click the right top profil icon")
+    public void click_the_right_top_profil_icon() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify that username should be seen under profil icon as expected")
+    public void verify_that_username_should_be_seen_under_profil_icon_as_expected() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
 
 }
